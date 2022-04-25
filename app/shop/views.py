5d609c2cc26d65f7798682630fba80ab69deaf37
swelanauguste <swelanauguste@gmail.com>
2022-04-25 15:07:30 -0400

    first template

---
+++ app/shop/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView
+
+from .models import Category, Product
+
+
+def product_list(request, category_slug=None):
+    category = None
+    category_list = Category.objects.all()
+    products_list = Product.objects.filter(is_available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products_list.filter(category=category_list)
+    return render(
+        request,
+        "shop/product_list.html",
+        {
+            "category": category,
+            "category_list": category_list,
+            "products_list": products_list,
+        },
+    )
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    # template_name = "shop/product_detail.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(ProductDetailView, self).get_context_data(**kwargs)
+    #     context["related_products"] = Product.objects.filter(category=self.object.category).exclude(pk=self.object.pk)
+    #     return context
